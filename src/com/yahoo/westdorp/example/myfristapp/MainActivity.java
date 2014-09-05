00c14b2e10b5cf2271b076f8ed1152a8490361c3
westdorp <westdorp@yahoo-inc.com>
2014-09-04 20:54:51 -0700

    initial commit

---
+++ src/com/yahoo/westdorp/example/myfristapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.yahoo.westdorp.example.myfristapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	public EditText etWords;
+	public TextView tvLabel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		etWords = (EditText) findViewById(R.id.etWords);
+		tvLabel = (TextView) findViewById(R.id.tvLabel);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void onSubmit(View v) {
+		String fieldValue = etWords.getText().toString();
+		//fire when the button is pressed
+		Toast.makeText(this, fieldValue, Toast.LENGTH_SHORT).show();
+		//change the text of the label
+		tvLabel.setText(fieldValue);
+	}
+
+}
